9a7b76133d82cdbd8dfd9b4bf79a1f94b8975c2f
Alistair Quinn <53567725+alistairjoelquinn@users.noreply.github.com>
2025-07-31 19:40:09 +0200

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,2 +1,5 @@
   nmcli device wifi list
   nmcli device wifi connect "YourNetworkName" password "YourPassword"
+  curl -fsSL
+  https://raw.githubusercontent.com/alistairjoelquinn/arch-setup-script/main/install.sh
+  | bash
